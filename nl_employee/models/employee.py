42d2f0d4eb47954f568f20ef0c34db0c6cefbfa6
janabdullahi <abdullahi@netlinks.af>
2023-06-25 09:37:57 +0430

    add employee mobile and email and add company which was required for the coach and make it invisible

--- nl_employee/models/employee.py
+++ nl_employee/models/employee.py
@@ -13,3 +13,8 @@ class employee(models.Model):
     job_id = fields.Many2one('hr.job', string="Job Title", related="employee_id.job_id")
     parent_id = fields.Many2one('hr.employee', string="Manager", related="employee_id.parent_id")
     category_ids = fields.Many2many('employee.category')
+    mobile_phone = fields.Char(related='employee_id.mobile_phone', readonly=False, related_sudo=False)
+    employee_phone = fields.Char(related='employee_id.phone', readonly=False, related_sudo=False)
+    work_email = fields.Char(related='employee_id.work_email', readonly=False, related_sudo=False)
+    coach_id = fields.Many2one(related='employee_id.coach_id', readonly=False, related_sudo=False)
+    company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company)
